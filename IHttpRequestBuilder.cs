03ed0ca0ae906fab80ecc9b74fe14d6182493bcf
Aaron Hammond <7657318+aaron-hammond@users.noreply.github.com>
2019-02-13 16:50:54 +0000

    Create IHttpRequestBuilder.cs

---
+++ IHttpRequestBuilder.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpRequestBuilder
+{
+    public interface IHttpRequestBuilder
+    {
+        IHttpRequestBuilder WithRequest(HttpMethod httpMethod, Uri resource);
+
+        IHttpRequestBuilder WithHeaders(Dictionary<string, string> headers);
+
+        IHttpRequestBuilder WithHeaders(Dictionary<string, IEnumerable<string>> headers);
+
+        IHttpRequestBuilder ConfigureAwait(bool configure);
+
+        IHttpRequestBuilder WithContent(object value, Encoding encoding, string mediaType = "application/json");
+
+        Task<HttpStatusResponse<T>> Execute<T>();
+    }
+}
